d637e6d53126819cf80e0f6b21dba2922234d2d8
Rajesh B <rajeshb@interfinet.com>
2021-01-03 19:25:10 +0530

    Chapter 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,27 +14,35 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Awesome App'),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(8),
-          alignment: Alignment.center,
-          width: 100,
-          height: 100,
-          child: Text('I am a Box', 
-            style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [BoxShadow(
-              color: Colors.black,
-              blurRadius: 5,
-              offset: Offset(7.0,5.0),
-            )],
-            color: Colors.red,
-            gradient: LinearGradient(
-              colors: [Colors.yellow, Colors.red]
-            )),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          ],
         ),
       ),
     );
